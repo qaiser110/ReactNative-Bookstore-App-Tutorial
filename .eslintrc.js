8dae3ad2791984ab03d2e1554f9cc25c32ec4ba4
Qaiser Abbas <2253847+qaiser110@users.noreply.github.com>
2018-05-10 16:42:18 +1000

    Added prettier and eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-    "extends": "airbnb"
-};
+  "parser": "babel-eslint",
+  "extends": [
+    "airbnb",
+    "prettier"
+  ],
+  "plugins": [
+    "prettier"
+  ],
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "trailingComma": "all",
+      }
+    ],
+    "react/prefer-stateless-function": "off",
+    "react/jsx-filename-extension": "off",
+    "no-use-before-define": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-curly-spacing": "off",
+    "react/jsx-equals-spacing": "off",
+    "react/jsx-first-prop-new-line": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-max-props-per-line": "off",
+    "react/jsx-tag-spacing": "off",
+    "react/jsx-wrap-multilines": "off"
+  }
+}
