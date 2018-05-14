013196c0542c74b19b8c558eff841e2e23a952ea
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2018-05-14 15:07:17 +1000

    Authors List using react-native-elements

--- src/views/author/index.js
+++ src/views/author/index.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import { View, Button } from 'react-native'
+import { ListItem } from 'react-native-elements'
 
 import Title from '../../components/Title'
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman',
+  },
+  {
+    name: 'Amanda Martin',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+    subtitle: 'CEO',
+  },
+  {
+    name: 'Christy Thomas',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    subtitle: 'Lead Developer',
+  },
+  {
+    name: 'Melissa Jones',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/nuraika/128.jpg',
+    subtitle: 'CTO',
+  },
+]
 
 export default ({ navigation }) => (
   <View>
     <Title text="Authors List" />
+    <View>
+      {list.map((l, i) => (
+        <ListItem
+          key={i}
+          leftAvatar={{ source: { uri: l.avatar_url } }}
+          title={l.name}
+          subtitle={l.subtitle}
+        />
+      ))}
+    </View>
     <Button onPress={() => navigation.openDrawer()} title="Open Drawer" />
     <Button onPress={() => navigation.navigate('Books')} title="Go to Books" />
   </View>
